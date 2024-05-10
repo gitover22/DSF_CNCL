3d873c3fd26df702d471b40b197445d263ac21d7
gitover22 <huafeng88@gmail.com>
2024-05-10 16:04:31 +0800

    add test

---
+++ test/test_cnclGetCommDevice.cc
@@ -0,0 +1,14 @@
+/***********************************************************************************
+ * author : huafeng
+ ***********************************************************************************/
+
+#include <cncl.h>
+#include <cnrt.h>
+#include <thread> 
+#include <vector>
+#include <iostream>
+#include "mluTool.h" // 需要配置.vscode文件添加自己的头文件搜索目录
+int main(int argc, char* argv[]) {
+   
+    return 0;
+}
